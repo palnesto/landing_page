129a8bcf6119fbd14492c236eb28b57492b90259
vamsikrishnamangu <mvvk96@gmail.com>
2023-08-26 14:46:39 +0530

    feat: added favicon and fixed whatsapp button with link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import Body from "./component/Body";
 import Bottom from "./component/Bottom";
+import FixedButton from "./component/FixedButton";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <div className="h-screen overflow-x-hidden">
+    <div className="relative h-screen overflow-x-hidden">
       <Navbar />
       <Body />
       <Bottom />
       <Footer />
+      <FixedButton />
     </div>
   );
 }
--- src/component/Button.js
+++ src/component/Button.js
@@ -3,20 +3,26 @@ import Button from "@mui/material/Button";
 
 export default function Buttons({ text }) {
   return (
-    <Button
-      sx={{
-        backgroundColor: "#F83A40",
-        color: "white",
-        width: "240px",
-        height: "48px",
-        fontWeight: "700",
-        borderRadius: "8px",
-        "&:hover": {
-          backgroundColor: "#F83A40",
-        },
-      }}
+    <a
+      href="//api.whatsapp.com/send?phone=919700739627&text=Hi, i would like to make a request"
+      target="_blank"
+      rel="noreferrer noopener"
     >
-      {text}
-    </Button>
+      <Button
+        sx={{
+          backgroundColor: "#F83A40",
+          color: "white",
+          width: "240px",
+          height: "48px",
+          fontWeight: "700",
+          borderRadius: "8px",
+          "&:hover": {
+            backgroundColor: "#F83A40",
+          },
+        }}
+      >
+        {text}
+      </Button>
+    </a>
   );
 }
