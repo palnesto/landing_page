2f751f7388f3ea515c1454f73f5540f639a3e660
Lilhorenikita <138877508+Lilhorenikita@users.noreply.github.com>
2023-09-05 18:16:55 +0530

    Update Body.js

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,18 +1,31 @@
 import React from "react";
-import Buttons from "./Button";
 
 const Body = () => {
   return (
     <div
       className="w-screen"
       style={{
-        backgroundImage: "url('/banner.png')",
         backgroundPosition: "center",
         backgroundSize: "cover",
         display: "flex",
         flexDirection: "column",
       }}
     >
+      <style>
+        {`
+          @media (min-width: 1024px) {
+            .w-screen {
+              background-image: url('/banner.png');
+            }
+          }
+          @media (max-width: 1023px) {
+            .w-screen {
+              background-image: url('/banners.png');
+              
+            }
+          }
+        `}
+      </style>
       <div className="flex flex-col justify-center items-center w-100">
         <div className="mt-20">
           <img src="./logo.png" alt="logo" />
@@ -20,19 +33,26 @@ const Body = () => {
         <p className="text-white font-Poppins text-base font-bold leading-9 text-center mt-10">
           GET YOUR ONLINE ID IN 5MINS
         </p>
-        <div className="mt-10 animate-pulse">
-          <Buttons text="WHATSAPP NOW" />
+        <div>
+          <a href="https://api.whatsapp.com/send?phone=+918297855853&text=Hello+I+want+a+ID+">
+            <img src="./Whatsapp.png" alt="WhatsApp logo" />
+          </a>
+        </div>
+        <div className="lg:flex flex-row mb-3">
+          <img src="./groupLogo.png" alt="logo" />
+          <img src="./Groups.png" alt="logo" />
+          <img src="./Grouped.png" alt="logo" />
         </div>
         <div className="flex flex-col items-center justify-center text-center">
-          <p className="lg:leading-9 text-5xl lg:text-8xl text-[#FFFFFF] mt-12 font-extrabold font-Poppins">
+          {/* <p className="lg:leading-9 text-5xl lg:text-8xl text-[#FFFFFF] mt-12 font-extrabold font-Poppins">
             TRUSTED{" "}
             <span className="text-5xl leading-tight lg:text-8xl text-[#F83A40] mt-10 font-extrabold font-Poppins">
               ONLINE <br /> CRICKET SITE
             </span>
-          </p>
+          </p> */}
         </div>
-        <div className="xl:mx-[10rem] lg:mt-16 mx-5">
-          <p className="w-100 my-20 font-poppins text-white text-center text-2xl font-bold ">
+        <div className="xl:mx-[10rem] lg:mt-16 mx-5 mt-52">
+          <p className="w-100 my-20 font-poppins text-white text-center text-sm lg:text-base font-bold">
             India's no. 1 online sports games promoter. Get your ID within 2
             minutes! Join Most Trusted, Secure & Fastest Book of India.
           </p>
