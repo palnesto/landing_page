76e7e2d1da49375ce546f1013129df3e94d36614
Lilhorenikita <138877508+Lilhorenikita@users.noreply.github.com>
2023-09-06 12:53:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    extend: {
+      keyframes: {
+        blink: {
+          "0%": { opacity: 1 },
+          "50%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        blink: "blink 1s infinite",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
